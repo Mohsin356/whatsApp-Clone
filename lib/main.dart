33cd7b3b2e54995eeb172fb9c554e4a93e1b9355
Mohsin.Raza <Mohsin356>
2022-05-13 20:14:53 +0500

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:whatsappclone/views/screens/agreeTerms.dart';
+import 'package:easy_splash_screen/easy_splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -15,7 +15,22 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Whatsapp',
       debugShowCheckedModeBanner: false,
-      home: AgreeTerms(),
+      home: EasySplashScreen(
+        logo: Image.asset('assets/logo.png'),
+          title:  const Text(
+            'WhatsApp',
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+                fontFamily: 'assets/fonts/overpass_regular.otf'),
+          ),
+          backgroundColor:  const Color(0xff075E54),
+          showLoader: true,
+          loadingText: const Text("Loading..."),
+          navigator: AgreeTerms(),
+          durationInSeconds: 5,
+      ),
+
     );
   }
 }
